36def294f2ab756e73d3d3f358e3b08bcde719d3
Jilesh980 <jilesh9034@gmail.com>
2023-11-28 15:07:10 -0500

    token verification upgraded

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,44 @@
-import { User } from "../models/user.js"
-import jwt from "jsonwebtoken"
-export const isAuthenticated = async(req,res,next)=>{
+import { User } from "../models/user.js";
+import jwt from "jsonwebtoken";
+
+export const isAuthenticated = async (req, res, next) => {
     try {
-        const {token} = req.cookies
-    // console.log(token)
-    if(!token){
-        return res.status(404).json({
-            success:false,
-            message:"Please login first"
-        })
-    }
-    const decoded = jwt.verify(token,process.env.JWTTOKEN)
-    req.user = await User.findById(decoded)
-    next()
+        const { token } = req.cookies;
+
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "Please log in first",
+            });
+        }
+
+        const jwtToken = process.env.JWTTOKEN || 'defaultSecret';
+        const decoded = jwt.verify(token, jwtToken);
+
+        req.user = await User.findById(decoded);
+
+        next();
     } catch (error) {
-        next(new Error(error))
+        if (error instanceof jwt.TokenExpiredError) {
+            return res.status(401).json({
+                success: false,
+                message: "Token has expired",
+            });
+        }
+
+        if (error instanceof jwt.JsonWebTokenError) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid token",
+            });
+        }
+
+        // Log the error for further investigation
+        console.error(`Authentication error: ${error.message}`);
+        
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        });
     }
-    
-}
+};
